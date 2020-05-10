e4866eaced25d19699617c673781b36c07b8a48f
John-Oula <johnoula@icloud.com>
2020-05-11 01:07:32 +0800

    Timestamp

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1116,7 +1116,7 @@ def getSimpleSign(source, SecretId, SecretKey):
 def createMeeting(username):
     SecretId = 'JIRMZ6O3Qm5KDwCHsgYnlxatGeXq7dfFcjEk'  # `SecretId` in key pair
     SecretKey = 'wZn5NeGCqxg4r8XaDum2EMzRhIvWHtcU'  # `SecretKey` in key pair
-    dateTime = datetime.datetime.utcnow().strftime(GMT_FORMAT)
+#    dateTime = datetime.datetime.utcnow().strftime(GMT_FORMAT)
     headerString = {"X-TC-Key" : str(SecretKey) , "X-TC-Nonce" : str(1234567) , "X-TC-Timestamp" : str(timeStamp),"content-type":"application/json","AppId":str(appID)}
 
     user = User.query.filter_by(username=username).first_or_404()

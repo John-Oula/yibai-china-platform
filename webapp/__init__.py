dd6263520bbf29a13a30bdf754dadbf807e5403c
John-Oula <johnoula@icloud.com>
2020-05-14 12:27:48 +0800

    auth

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1188,7 +1188,7 @@ def test(username):
     print(generateHeaders('GET','',uri))
     auth = generateHeaders('GET','',uri)
 
-    response = requests.get(url,headers=generateHeaders('GET','',uri),auth=HTTPBasicAuth(auth['X-TC-KEY'],auth['X-TC-SIGNATURE']))
+    response = requests.get(url,headers=generateHeaders('GET','',uri),auth=HTTPBasicAuth(auth['X-TC-Key'],auth['X-TC-Signature']))
 
     print(response.url)
     return response.json()

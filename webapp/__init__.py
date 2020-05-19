b0d0ed7509d07aacabcdd08fba10e23365c02326
John-Oula <johnoula@icloud.com>
2020-05-20 05:09:07 +0800

    meeting ID col

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -181,6 +181,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column('id', db.Integer, primary_key=True)
+    meetingId = db.Column('MeetingId', db.Integer,nullable=False)
     verified = db.Column('verified', db.Integer, default=0, nullable=True)
     title = db.Column('title', db.String(70), nullable=True)
     category = db.Column('category', db.String(10), nullable=True)
@@ -192,9 +193,10 @@ class Post(db.Model):
     end_time = db.Column('End time', db.String, nullable=True)
     lesson = db.relationship('Lesson', backref=db.backref('lessons'))
 
-    def __repr__(self, id, verified, title, category, description, files, date, user_id, start_time,
+    def __repr__(self, id,meetingId, verified, title, category, description, files, date, user_id, start_time,
                  end_time):
         self.id = id
+        self.meetingId = meetingId
         self.verified = verified
         self.title = title
         self.category = category
@@ -922,8 +924,40 @@ def createMeeting(title,fulltime,end_time):
     print(r.json())
 
     return r.json()
-def cancelMeeting():
-    return
+def cancelMeeting(meetingId):
+    num = random.randint(0, 999999999)
+    stamp = int(time.time())
+
+    uri = "/v1/meetings"
+
+    headerString = "X-TC-Key=%s&X-TC-Nonce=%s&X-TC-Timestamp=%s" % (SecretId, num, str(stamp))
+    req_body = {
+     "userid" : str(current_user.username),
+     "instanceid" : 1,
+     "reason_code" : 1,
+     "reason_detail" : "none"
+    }
+    req_body = json.dumps(req_body)
+    stringToSign = "%s\n%s\n%s\n%s" % ('POST', headerString, uri, req_body)
+    print(stringToSign)
+
+    your_secretkey = SecretKey.encode('utf-8')
+    stringToSign = stringToSign.encode('utf-8')
+
+    signature = hmac.new(your_secretkey, stringToSign, digestmod=hashlib.sha256).hexdigest()
+    print(signature)
+
+    signature = base64.b64encode(signature.encode("utf-8"))
+
+    headers = {'Content-Type': 'application/json', 'X-TC-Key': SecretId, 'X-TC-Timestamp': str(stamp),
+               'X-TC-Nonce': str(num), 'AppId': '200000164', 'X-TC-Signature': signature, 'X-TC-Registered': '0'}
+    datas = req_body
+    r = requests.post("https://api.meeting.qq.com/v1/meetings/%s/cancel" % (meetingId), data=datas, headers=headers)
+    print(r.text)
+    print(r.json())
+
+    return r.json()
+
 
 
 @app.route('/create/<username>',methods=['GET','POST'])

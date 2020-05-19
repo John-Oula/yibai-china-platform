5d1a655c44333434fb908bbd2af4319f6601966a
John-Oula <johnoula@icloud.com>
2020-05-20 02:02:58 +0800

    create meeting

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -950,7 +950,7 @@ def create(username):
                      nationality = verify_form.nationality.data,occupation = verify_form.occupation.data,email = verify_form.email.data,phone = verify_form.phone.data)
 
         db.session.add(post,verify)
-        createMeeting(current_user.id,form.title.data,time,end_time)
+        createMeeting(current_user.id,form.title.data,time%60,end_time%60)
 #        db.session.commit()
 
         return redirect(url_for('user_profile',username=current_user.username))

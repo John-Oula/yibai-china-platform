0258b77fb3c28f9fdc088f3707f3a92fb98b4490
John-Oula <johnoula@icloud.com>
2020-05-20 11:39:47 +0800

    meeting page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1000,7 +1000,7 @@ def create(username):
 
         db.session.commit()
 
-        return redirect(url_for('meetingInfo',meetingCode))
+        return redirect(url_for('meetingInfo',meetingcode=meetingCode))
     return render_template('CREATE1.html',user=user,user_role = user_role,form=form,verify_form=verify_form,lesson_form=lesson_form,image_file=image_file)
 
 

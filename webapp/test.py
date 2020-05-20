2b1453cef34e7cacafaa8a33c73473fb2929f295
John-Oula <johnoula@icloud.com>
2020-05-20 13:53:02 +0800

    meetings cancel

--- webapp/test.py
+++ webapp/test.py
@@ -11,34 +11,39 @@ import requests
 SecretId = "JIRMZ6O3Qm5KDwCHsgYnlxatGeXq7dfFcjEk"
 SecretKey = "wZn5NeGCqxg4r8XaDum2EMzRhIvWHtcU"
 
-num = random.randint(0,999999999)
-stamp = int(time.time())
+def inquire():
+    num = random.randint(0, 999999999)
+    stamp = int(time.time())
+    meet=985966732
+    userid='user'
+    i=1
 
-uri = "/v1/meetings/12269259670683052373/cancel"
+    uri = "/v1/meetings?meeting_code="+str(meet)+"&userid="+userid+"&instanceid="+str(i)
 
-headerString = "X-TC-Key=%s&X-TC-Nonce=%s&X-TC-Timestamp=%s" % (SecretId,num,str(stamp))
+    headerString = "X-TC-Key=%s&X-TC-Nonce=%s&X-TC-Timestamp=%s" % (SecretId, num, str(stamp))
 
-req_body = {
-    "userid": 'user',
-    "instanceid": 1,
-    "reason_code": 1,
-}
-req_body = json.dumps(req_body)
-stringToSign = "%s\n%s\n%s\n%s" % ('POST', headerString, uri, req_body)
-print(stringToSign)
+    req_body = ""
 
-your_secretkey = SecretKey.encode('utf-8')
-stringToSign = stringToSign.encode('utf-8')
+    stringToSign = "%s\n%s\n%s\n%s" % ('GET', headerString, uri, req_body)
+    print(stringToSign)
 
-signature = hmac.new(your_secretkey, stringToSign, digestmod=hashlib.sha256).hexdigest()
-print(signature)
+    your_secretkey = SecretKey.encode('utf-8')
+    stringToSign = stringToSign.encode('utf-8')
 
-signature = base64.b64encode(signature.encode("utf-8"))
+    signature = hmac.new(your_secretkey, stringToSign, digestmod=hashlib.sha256).hexdigest()
+    print(signature)
 
-headers = {'Content-Type': 'application/json', 'X-TC-Key': SecretId, 'X-TC-Timestamp': str(stamp),
-           'X-TC-Nonce': str(num), 'AppId': '200000164', 'X-TC-Signature': signature, 'X-TC-Registered': '0'}
-datas = req_body
-r = requests.post("https://api.meeting.qq.com/v1/meetings/12269259670683052373/cancel", data=datas, headers=headers)
-print(r.ok)
-print(r.json())
+    signature = base64.b64encode(signature.encode("utf-8"))
+    params = {
+        "meeting_code": '985966732',
+        "userid": 'user',
+        "instanceid": 1,
+    }
 
+    headers = {'Content-Type': 'application/json', 'X-TC-Key': SecretId, 'X-TC-Timestamp': str(stamp),
+               'X-TC-Nonce': str(num), 'AppId': '200000164', 'X-TC-Signature': signature, 'X-TC-Registered': '0'}
+    r = requests.get("https://api.meeting.qq.com/v1/meetings", params=params, headers=headers)
+    print(r.text)
+    return r.json()
+
+inquire()

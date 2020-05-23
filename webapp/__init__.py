082fac950441e5357fe9c1840cfc9d375e1f3cf5
John-Oula <johnoula@icloud.com>
2020-05-24 01:54:28 +0800

    hash pass random=500

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -455,7 +455,7 @@ def corona():
 
 def hash_password(password):
     """Hash a password for storing."""
-    salt = hashlib.sha256(os.urandom(40)).hexdigest().encode('ascii')
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
     pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
                                 salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)

66ccec6764f4d57ef067ff5a643f6325a9762a25
John-Oula <johnoula@icloud.com>
2020-05-24 18:04:16 +0800

    modify function

--- webapp/test.py
+++ webapp/test.py
@@ -10,18 +10,39 @@ import requests
 
 SecretId = "JIRMZ6O3Qm5KDwCHsgYnlxatGeXq7dfFcjEk"
 SecretKey = "wZn5NeGCqxg4r8XaDum2EMzRhIvWHtcU"
-
+id='16687243626258567007'
 def inquire():
     num = random.randint(0, 999999999)
     stamp = int(time.time())
 
-    uri = "/v1/meetings?meeting_code=985966732&userid=user&instanceid=1"
+    uri = "/v1/meetings/%s" % (id)
 
     headerString = "X-TC-Key=%s&X-TC-Nonce=%s&X-TC-Timestamp=%s" % (SecretId, num, str(stamp))
 
-    req_body = ""
+    req_body = {
+        "userid": 'olduser',
+        "instanceid": 1,
+        "subject": 'modify',
+        "type": 0,
+        "hosts": [{"userid": 'olduser'}],
 
-    stringToSign = "%s\n%s\n%s\n%s" % ('GET', headerString, uri, req_body)
+        "start_time": str(stamp+3000),
+        "end_time": str(stamp+6000),
+        "settings": {
+            "mute_enable_join": True,
+            "allow_unmute_self": False,
+            "mute_all": False,
+            "host_video": True,
+            "participant_video": False,
+            "enable_record": False,
+            "play_ivr_on_leave": False,
+            "play_ivr_on_join": False,
+            "live_url": False
+        }
+    }
+    req_body = json.dumps(req_body)
+
+    stringToSign = "%s\n%s\n%s\n%s" % ('PUT', headerString, uri, req_body)
     print(stringToSign)
 
     your_secretkey = SecretKey.encode('utf-8')
@@ -31,15 +52,11 @@ def inquire():
     print(signature)
 
     signature = base64.b64encode(signature.encode("utf-8"))
-    params = {
-        "meeting_code": '985966732',
-        "userid": 'user',
-        "instanceid": 1,
-    }
+    datas = req_body
 
     headers = {'Content-Type': 'application/json', 'X-TC-Key': SecretId, 'X-TC-Timestamp': str(stamp),
                'X-TC-Nonce': str(num), 'AppId': '200000164', 'X-TC-Signature': signature, 'X-TC-Registered': '0'}
-    r = requests.get("https://api.meeting.qq.com/v1/meetings", params=params, headers=headers)
+    r = requests.put("https://api.meeting.qq.com/v1/meetings/%s" % (id),data=datas,headers=headers)
     print(r.text)
     return r.json()
 

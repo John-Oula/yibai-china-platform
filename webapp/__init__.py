a6749fa97db807534b19c0f1a198f92627808112
John-Oula <johnoula@icloud.com>
2020-06-19 18:20:36 +0800

    new function series

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1462,6 +1462,7 @@ def upload(username,id):
             series = Series(title=seriesForm.title.data, description=seriesForm.description.data,
                             category=seriesForm.category.data, price=seriesForm.price.data, user_series=current_user)
             db.session.add(series)
+
             episode = Episode(subtitle=episodeForm.subtitle.data, description=episodeForm.description.data,
                               upload_ref=videoPath,transcript_ref= transcriptPath,auido_ref=audioPath,user_episode=current_user, series_id=id)
             db.session.add(episode)
@@ -1478,8 +1479,9 @@ def upload(username,id):
             series = Series(title=seriesForm.title.data, description=seriesForm.description.data,
                             category=seriesForm.category.data, price=seriesForm.price.data, user_series=current_user)
             db.session.add(series)
+            db.session.flush()
             episode = Episode(subtitle=episodeForm.subtitle.data, description=episodeForm.description.data,
-                              upload_ref=videoPath,auido_ref = audioPath,transcript_ref=transcriptPath, user_episode=current_user, series_id=id)
+                              upload_ref=videoPath,auido_ref = audioPath,transcript_ref=transcriptPath, user_episode=current_user)
             db.session.add(episode)
 
             db.session.commit()

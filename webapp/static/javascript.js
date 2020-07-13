bbd9d9cf4e383956acbbf89d8c4d54d503c6e258
John-Oula <johnoula@icloud.com>
2020-07-13 16:01:29 +0800

    mobile

--- webapp/static/javascript.js
+++ webapp/static/javascript.js
@@ -380,3 +380,22 @@ $('#session-content').css('display','none');
   });
   });
 
+
+
+
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+
+

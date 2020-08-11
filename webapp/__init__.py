f3c7625191d8f6a378378bdc0da3fa3d2abfc351
John-Oula <johnoula@icloud.com>
2020-08-11 16:57:10 +0800

    'abspath'

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -58,11 +58,11 @@ def get_Host_name_IP(hostname):
 
 #alipay setup
 
-app_private_key_string = open(os.path.abspath('appPrivateKey.txt')).read()
-alipay_public_key_string = open(os.path.abspath('alipayPublicKey.txt')).read()
-app_public_key_cert_string = open(os.path.abspath("appCertPublicKey_2021001182663949.crt")).read()
-alipay_root_cert_string = open(os.path.abspath("alipayRootCert.crt")).read()
-alipay_public_key_cert_string = open(os.path.abspath("alipayCertPublicKey_RSA2.pem")).read()
+app_private_key_string = open(os.path.abspath('keys/appPrivateKey.txt')).read()
+alipay_public_key_string = open(os.path.abspath('keys/alipayPublicKey.txt')).read()
+app_public_key_cert_string = open(os.path.abspath("certs/appCertPublicKey_2021001182663949.crt")).read()
+alipay_root_cert_string = open(os.path.abspath("certs/alipayRootCert.crt")).read()
+alipay_public_key_cert_string = open(os.path.abspath("certs/alipayCertPublicKey_RSA2.pem")).read()
 
 
 

a0c0ef6cfee625b87a1243396cdfaf341167d9b7
John-Oula <johnoula@icloud.com>
2020-08-16 13:46:19 +0800

    'audio'

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1427,7 +1427,7 @@ def videoDetails():
         return jsonify({'result':data})
     else:
         pass
-    pass
+    return '', 204
 
 @app.route('/liveDetails' , methods=['POST','GET'])
 def liveDetails():
